fa7fd6569137496a383116380b3af5df646d90b8
CodeClan Student <7378035+NodeToNowhere@users.noreply.github.com>
2021-12-03 19:36:28 +0000

    filling out base

---
+++ db/expense_tracker.sql
@@ -0,0 +1,16 @@
+DROP TABLE transactions;
+DROP TABLE tags;
+DROP TABLE merchants;
+CREATE TABLE merchants(
+    id SERIAL PRIMARY KEY,
+    merchant VARCHAR(63)
+);
+CREATE TABLE tags(id SERIAL PRIMARY KEY, tag VARCHAR(63));
+CREATE TABLE transactions(
+    id SERIAL PRIMARY KEY,
+    amount INT,
+    date DATETIME,
+    description TEXT,
+    merchant_id INT REFERENCES merchants(id) ON DELETE CASCADE,
+    tag_id INT REFERENCES tags(id) ON DELETE CASCADE
+);
