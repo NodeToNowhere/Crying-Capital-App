1a477001556d87a9ae4bc26ced6f80143be72b5b
CodeClan Student <7378035+NodeToNowhere@users.noreply.github.com>
2021-12-05 00:58:20 +0000

    Basic CRUD working

--- console.py
+++ console.py
@@ -38,13 +38,14 @@ print(tag_rep.select(2))
 print(tag_rep.select_all)
 
 
-tran1 = Transaction(45.55, "12/12/12", "Cigarettes and wine", mer2, tag3)
+tran1 = Transaction(45.55, "12-12-12", "Cigarettes and wine", mer2, tag3)
 tran_rep.save(tran1)
 
-tran2 = Transaction(12.22, "11/11/11,", "Bus to funeral", mer3,tag2)
+tran2 = Transaction(12.22, "11-11-11", "Bus to funeral", mer3,tag2)
 tran_rep.save(tran2)
 
-tran2 = Transaction(23.12, "9/9/09", "Spam and juice", mer1, tag1)
+tran3 = Transaction(23.12, "9-9-99", "Spam and juice", mer1, tag1)
 tran_rep.save(tran2)
 
+tran4 = Transaction(11.11, "8-8-88", "Monthly debt repayments")
 pdb.set_trace()
