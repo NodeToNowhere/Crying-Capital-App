c77f923f337c93a842a7b49164563ee5022b99ec
CodeClan Student <7378035+NodeToNowhere@users.noreply.github.com>
2021-12-06 22:16:00 +0000

    Added flask tracer to hunt this thing down

--- app.py
+++ app.py
@@ -2,8 +2,15 @@ from flask import Flask, render_template
 from controllers.merchant_controller import merchant_blueprint
 from controllers.tags_controller import tags_blueprint
 from controllers.transaction_controller import transaction_blueprint
+import logging
 
 app = Flask(__name__)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format=f"%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s",
+)
+
 
 app.register_blueprint(merchant_blueprint)
 app.register_blueprint(tags_blueprint)
@@ -12,6 +19,8 @@ app.register_blueprint(transaction_blueprint)
 
 @app.route("/")
 def home():
+    app.logger.info("Info level log")
+    app.logger.warning("Warning level log")
     return render_template("index.html", test="THIS IS A TEST")
 
 
@@ -19,4 +28,4 @@ if __name__ == "__main__":
     app.run(debug=True)
 
 
-#ask/find info about why I have to route '/' to app. 
+# ask/find info about why I have to route '/' to app.
